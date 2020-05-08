f29d5cc06b7e6f5e29f4b4649ecfa7dda02260c5
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-08 14:35:28 -0600

    added delete route and function

--- Develop/routes/api/apiRoutes.js
+++ Develop/routes/api/apiRoutes.js
@@ -26,6 +26,30 @@ routerAPI.post('/notes', (req, res) => {
             console.log('File write successful!')
         });
     });
+
+    res.sendStatus(200);
+})
+
+// API DELETE Routes
+routerAPI.delete('/notes/:id', (req, res) => {
+    console.log('Delete Hit id: ' + req.params.id)
+
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const targetId = req.params.id;
+        const dataArray = JSON.parse(data);
+
+        let newDataArray = dataArray.filter((obj) => {
+            return obj.id != targetId;
+        });
+
+        fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(newDataArray), (err) => {
+            if (err) throw err;
+            console.log('File write successful!')
+        });
+    });
+
+    res.sendStatus(200);
 })
 
 module.exports = routerAPI;
